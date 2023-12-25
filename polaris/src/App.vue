a6e234bfb56a336248b93b79f7833de070569874
sighhh <sighhh0704@gmail.com>
2023-12-25 17:44:03 +0800

    # 部分修改

--- polaris/src/App.vue
+++ polaris/src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <div class="container">
-    <header> {{ title }} </header>
+    <header class="nav"> {{ title }} </header>
     <div class="main">
-      <div class="main-content">
-        <router-view></router-view>
-      </div>
-      <aside class="sidebar sidebar-left">Left Sidebar</aside>
-      <aside class="sidebar sidebar-Right">Right Sidebar</aside>
+      <div class="left">Left</div>
+      <div class="right">Right</div>
+      <div class="center">Center</div>
     </div>
-    <footer>Footer</footer>
+    <footer>{{ welecome }}</footer>
   </div>
 </template>
 
 <script>
-
 export default {
-  name:'App',
   data() {
     return {
       title: "Polaris",
       welecome: "Hello! Welecome to Polaris !",
-      dynamicID: "AppID",
-      dynamicClass: "AppClass",
-      isBtnDisabled: false
     }
   }
 }
 </script>
-
-<style>
-.AppClass {
-  color: rgb(59, 47, 122);
-  font-size: 50px;
-}
-</style>
