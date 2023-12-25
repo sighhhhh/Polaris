d07a8e3f4ae6a980d59fe4cf1a03c5046909a878
sighhh <sighhh0704@gmail.com>
2023-12-26 05:28:19 +0800

    美化菜单栏

--- polaris/src/App.vue
+++ polaris/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
-    <header class="nav"> {{ title }} </header>
-    <div class="main">
-      <div class="left">Left</div>
-      <div class="right">Right</div>
-      <div class="center">Center</div>
-    </div>
+    <header>
+      <div class="project-name"> {{ title }} </div>
+      <ul>
+        <li><a href="">Support</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact us</a></li>
+      </ul>
+    </header>
+    <div class="divider"></div>
+
     <footer>{{ welecome }}</footer>
   </div>
 </template>
@@ -15,7 +19,7 @@ export default {
   data() {
     return {
       title: "Polaris",
-      welecome: "Hello! Welecome to Polaris !",
+      welecome: "Welecome to Polaris !",
     }
   }
 }
--- polaris/src/main.js
+++ polaris/src/main.js
@@ -3,6 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/layout/layout.css'
+import './assets/layout/divider.css'
+import './assets/layout/menu.css'
+
 import './assets/style/box/box_25.css'
 
 import './assets/style/button/btn_29.css'
