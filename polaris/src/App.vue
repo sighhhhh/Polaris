5c1ab28614300992f455558f2a0ea64876751752
sighhh <sighhh0704@gmail.com>
2023-12-30 09:13:09 +0800

    side-menu-box-upgrade

--- polaris/src/App.vue
+++ polaris/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <header>
-    <ul>
-      <li><a href="">Support</a></li>
-      <li><a href="">About</a></li>
-      <li><a href="">Contact</a></li>
-    </ul>
-    <div class="project-name"> {{ title }} </div>
+    <title-ul>
+      <title-li><a href="">Support</a></title-li>
+      <title-li><a href="">About</a></title-li>
+      <title-li><a href="">Contact</a></title-li>
+    </title-ul>
+    <div class="project-name" style="background-color: white;"> {{ title }} </div>
+    <div class="divider"></div>
   </header>
-  <div class="divider"></div>
-  <div class="sidebar">
+
+  <div class="sidebar side-nav box-shadow-25">
     <div class="project-name">Menu</div>
-    <ul>
-      <li><a href="">HOME</a></li>
-      <li><a href="">Combine</a></li>
-      <li><a href="">Carrot</a></li>
-      <li><a href="">Polish Characters</a></li>
+    <ul style="padding-left: 0;">
+      <menu-li><a href="">HOME</a></menu-li>
+      <menu-li><a href="">Combine</a></menu-li>
+      <menu-li><a href="">Carrot</a></menu-li>
+      <menu-li><a href="">Polish Characters</a></menu-li>
     </ul>
   </div>
+
+  <main-area>
+    <p>123456789</p>
+  </main-area>
+
+
   <footer>{{ welecome }}</footer>
 </template>
 
@@ -25,7 +32,7 @@ export default {
   data() {
     return {
       title: "Polaris",
-      welecome: "Welecome to Polaris !",
+      welecome: "Welecome to Polaris ! sighhh0704@gmial.com",
     }
   }
 }
--- polaris/src/main.js
+++ polaris/src/main.js
@@ -6,6 +6,7 @@ import './assets/layout/layout.css'
 import './assets/layout/divider.css'
 import './assets/layout/menu.css'
 import './assets/layout/sidebar.css'
+import './assets/layout/mainarea.css'
 
 import './assets/style/box/box_25.css'
 
@@ -15,3 +16,4 @@ import './assets/style/button/btn_85.css'
 import './assets/style/button/btn_86.css'
 
 createApp(App).mount('#app')
+
