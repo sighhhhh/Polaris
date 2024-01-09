60865c59c361c1a720e0ea8ecf82239c5ebc72d9
sighhh <sighhh0704@gmail.com>
2024-01-09 13:40:45 +0800

    ok

--- polaris/src/main.js
+++ polaris/src/main.js
@@ -3,9 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/layout/layout.css'
-import './assets/layout/divider.css'
-import './assets/layout/menu.css'
-import './assets/layout/sidebar.css'
+import './assets/style/divider/divider.css'
+import './assets/style/menu/menu.css'
+import './assets/style/sidebar/sidebar.css'
 import './assets/layout/mainarea.css'
 
 import './assets/style/box/box_25.css'
@@ -16,4 +16,5 @@ import './assets/style/button/btn_85.css'
 import './assets/style/button/btn_86.css'
 
 createApp(App).mount('#app')
+
 
